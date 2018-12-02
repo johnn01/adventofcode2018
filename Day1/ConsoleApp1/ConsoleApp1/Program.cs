3c36b0e7cd52c6c8fa71fdbf1e887151cdbe58b8
John <mobab.mobab@gmail.com>
2018-12-02 13:17:32 -0600

    day1-part1

---
+++ Day1/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Xml.Linq;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var part1 = GetInput()
+                .Select(int.Parse)
+                .Sum();
+
+
+            Console.ReadKey();
+        }
+
+        private static string[] GetInput()
+        {
+            using (var reader = File.OpenText("input.txt"))
+            {
+                var fileText = reader.ReadToEnd();
+                return fileText.Split(new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+            }
+        }
+
+    }
+}
